f938d780459914ad5611d5da15f828d659af38f8
Naveed Yahya-Zadeh <nyahyazadeh39@gmail.com>
2018-03-04 03:19:19 -0500

    Add files via upload

--- Chrome Extension/options.js
+++ Chrome Extension/options.js
@@ -14,3 +14,23 @@ window.addEventListener('DOMContentLoaded', function() {
         chrome.tabs.create({ url: newURL });
     });
 });
+
+var dial = $(".dial .inner");
+	var gauge_value = $(".gauge .value");
+
+		function rotateDial()
+		{
+			var deg = 0;
+			var value = Math.round(Math.random()*100);
+			deg = (value * 177.5) / 100;
+
+			gauge_value.html(value + "%");
+
+			dial.css({'transform': 'rotate('+deg+'deg)'});
+		    dial.css({'-ms-transform': 'rotate('+deg+'deg)'});
+		    dial.css({'-moz-transform': 'rotate('+deg+'deg)'});
+		    dial.css({'-o-transform': 'rotate('+deg+'deg)'}); 
+		    dial.css({'-webkit-transform': 'rotate('+deg+'deg)'});
+		}
+
+		setInterval(rotateDial, 2000);
