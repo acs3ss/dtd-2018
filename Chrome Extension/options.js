9450706838ecfccfc255986dce5e312e92eaad3f
Naveed Yahya-Zadeh <nyahyazadeh39@gmail.com>
2018-03-04 11:06:50 -0500

    Add files via upload

--- Chrome Extension/options.js
+++ Chrome Extension/options.js
@@ -1,16 +1,33 @@
+
 window.addEventListener('DOMContentLoaded', function() {
     // your button here
-    var link = document.getElementById('btnOpenNewTab');
+	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+	console.log(url);
+});
+    var link = document.getElementById('btnOpenNewTab1');
     // onClick's logic below:
     link.addEventListener('click', function() {
-        var newURL = "http://stackoverflow.com/";
+        var newURL = "https://www.washingtonpost.com/regional/";
         chrome.tabs.create({ url: newURL });
     });
 	// your button here
     var link = document.getElementById('btnOpenNewTab2');
     // onClick's logic below:
     link.addEventListener('click', function() {
+        var newURL = "https://stackoverflow.com/";
+        chrome.tabs.create({ url: newURL });
+    });
+	    var link = document.getElementById('btnOpenNewTab3');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
         var newURL = "https://www.nytimes.com/";
+        chrome.tabs.create({ url: newURL });
+    });
+	    var link = document.getElementById('btnOpenNewTab4');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+        var newURL = "https://www.breitbart.com/";
         chrome.tabs.create({ url: newURL });
     });
 });
